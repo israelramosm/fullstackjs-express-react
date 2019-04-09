5812ed933751938b1c0cc748bb76833143ac6dfc
Israel-94 <arnulfo_israel@hotmail.com>
2019-04-08 19:44:22 -0500

    prodMode and process ENV using dotenv

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
-// TODO: Add an if for prod or dev modes
+import dotenv from 'dotenv';
+
 import devApp, { staticFiles } from "./server-dev";
 
-// should receive the port from env
-// console.log(process.env.PORT);
+// importing enviroment variables from .env
+dotenv.config();
 
 // should receive de mode from env
-const prodMode = false, //process.env.mode == 'production', 
+const prodMode = process.env.NODE_ENV == "production", //process.env.mode == 'production', 
     app = prodMode ? express() : devApp(),
     PORT =  process.env.PORT || 8080,
     DIST_DIR = __dirname + "/public",
     HTML_FILE = path.join(DIST_DIR, './public/index.html');
+
 
 /* Configuration */
 app.use(bodyParser.json());  
@@ -29,16 +31,13 @@ app.get("/api", (req, res) => {
 /* Static Files */
 app.use(express.static(DIST_DIR));
 
-// Check for production to use this line
+// Check for production to use just the static files required
 if(prodMode){
-    console.log("Production")
     app.get('*', (req, res) => {
-        console.log("Index.html");
-        console.log(HTML_FILE);
         res.sendFile(HTML_FILE)
     })
-// Using static files from development mode to webpack hot reload    
-}else {
+} else {
+    // Using static files from development mode to webpack hot reload    
     staticFiles(app);
 }
 
