b48364c1157469606f0e5e43c0bac1052e3603b4
Israel Ramos <arnulfo_israel@hotmail.com>
2019-08-12 00:46:22 -0500

    fix: test and environments for node, added mongo mock, user test for singup

--- __mocks__/mockapp.js
+++ __mocks__/mockapp.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const initServer =  (apiFunc, base_url) => {
   const app = express()
   const route = express.Router()
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
 
   apiFunc(app, base_url, route)
 
--- __tests__/server.tests/user.test.js
+++ __tests__/server.tests/user.test.js
@@ -1,14 +1,31 @@
 import moxios from 'moxios'
 import request from 'supertest'
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose'
 
 import initServer from '../../__mocks__/mockapp'
 import base from '../../src/server/routes/base'
 
 const BASE_URL = '/api'
+const opts = { useCreateIndex: true, useNewUrlParser: true } 
 
 describe('User Auth tests', () => {
   let server
+  let mongoServer
 
+  beforeAll(async () => {
+    mongoServer = new MongoMemoryServer();
+    const mongoUri = await mongoServer.getConnectionString();
+    await mongoose.connect(mongoUri, opts, (err) => {
+      if (err) console.error(err);
+    });
+  })
+  
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+  
   beforeEach(() => {
     moxios.install()
     server = initServer(base, BASE_URL)
@@ -26,10 +43,13 @@ describe('User Auth tests', () => {
   })
 
   test('POST /api/signup', async(done) => {
-    const res = await request(server).post(`${BASE_URL}/signup`)
-
-    expect(res.body.message).toEqual('Sucess! You are signup.')
-    done()
+    let res = await request(server)
+      .post(`${BASE_URL}/signup`)
+      .send({ email: 'example@domain.com', password: 'password' })
+      
+      expect(res.body.email).toEqual('example@domain.com')
+      expect(res.body.password).toEqual('password')
+      done();
   })
 
   test('GET /api/logout', async(done) => {
