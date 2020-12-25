e41ea7d719f9155a7ab06c2b738c8cc7530955df
Israel Ramos <arnulfo_israel@hotmail.com>
2020-12-25 11:28:25 -0700

    covergae config and test regex pattern

--- __tests__/client.config.js
+++ __tests__/client.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testMatch: [ "<rootDir>/client.tests/**/*.js" ],
+  testMatch: [ "<rootDir>/client.tests/**/*.test.js" ],
     moduleNameMapper: {
         "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
         "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
