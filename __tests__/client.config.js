194c211819205923324a7e2134f6466e8c687a03
Israel Ramos <arnulfo_israel@hotmail.com>
2020-12-25 10:46:41 -0700

    updating client tests config

--- __tests__/client.config.js
+++ __tests__/client.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testMatch: [ "<rootDir>/client.tests/**/*" ],
+  testMatch: [ "<rootDir>/client.tests/**/*.js" ],
     moduleNameMapper: {
         "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
         "\\.(gif|ttf|eot|svg)$": "<rootDir>/__mocks__/fileMock.js"
       }
-}
+};
