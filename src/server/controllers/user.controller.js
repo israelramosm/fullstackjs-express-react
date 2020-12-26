f3dae9b406c2d57c874827b6355e8cde9a9ded4e
Israel Ramos <arnulfo_israel@hotmail.com>
2020-12-25 19:15:44 -0700

    working with mongodb, testing calls to API, added Constant file

--- src/server/controllers/user.controller.js
+++ src/server/controllers/user.controller.js
@@ -1,42 +1,43 @@
-import User from "../models/userModel"
+import { DEBUG } from "../../util/Constants";
+import User from "../models/userModel";
 
 /**
  * Should login to the application
  * POST /api/login
- * @param {*} req 
- * @param {*} res 
- */  
-  export const postLogin = (req, res) => {
-    let data = {
-      message: "Sucess! You are login."
-    }
-    res.status(200).send(data)
-  }
-  
-  /**
-   * Should signup the user to the application
-   * POST /api/signup
-   * @param {*} req 
-   * @param {*} res 
-   */
-  export const postSignup = (req, res) => {
-    let newUser = new User(req.body)
-    newUser.save((err, user) => {
-      if(err) res.json(err)
+ * @param {*} req
+ * @param {*} res
+ */
+export const postLogin = (req, res) => {
+  let data = {
+    message: "Sucess! You are login.",
+  };
+  res.status(200).send(data);
+};
 
-      res.json(user)
-    })
-  }
-  
-  /**
-   * Should logout the user from the application
-   * GET /api/logout
-   * @param {*} req 
-   * @param {*} res 
-   */
-  export const getLogout = (req, res) => {
-    let data = {
-      message: "Sucess! You are logout."
-    }
-    res.status(200).send(data)
-  }
+/**
+ * Should signup the user to the application
+ * POST /api/signup
+ * @param {*} req
+ * @param {*} res
+ */
+export const postSignup = (req, res) => {
+  let newUser = new User(req.body);
+  newUser.save((err, user) => {
+    if (err) res.status(500).send(err);
+
+    res.status(200).send(user);
+  });
+};
+
+/**
+ * Should logout the user from the application
+ * GET /api/logout
+ * @param {*} req
+ * @param {*} res
+ */
+export const getLogout = (req, res) => {
+  let data = {
+    message: "Sucess! You are logout.",
+  };
+  res.status(200).send(data);
+};
