95d16e162dd06342c9dd135cac0800998dd0200f
Israel Ramos <arnulfo_israel@hotmail.com>
2021-02-04 23:32:44 -0700

    logic to expired tokens, calls to endpoints updated

--- src/server/controllers/auth.controller.js
+++ src/server/controllers/auth.controller.js
@@ -41,7 +41,8 @@ export const postLogin = async (req, res) => {
         id: user._id,
         email: user.email,
       },
-      process.env.TOKEN_SECRET
+      process.env.TOKEN_SECRET,
+      { expiresIn: 60 * 60 } // Expire in 1 hour
     );
 
     return res
--- src/server/controllers/middleware/jwt_service.js
+++ src/server/controllers/middleware/jwt_service.js
@@ -5,9 +5,18 @@ const verifyToken = (req, resp, next) => {
   if (!token) return resp.status(401).json({ error: "Access denied" });
 
   try {
-      const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-      req.user = verified;
-      next();
+    // Epoch time for 1 hour 3600
+    // Validation to denied acces to expired tokens
+    // Maybe it would more secure if I create a blacklist of tokens in the db
+    const decoded = jwt.decode(token);
+    const expiration = decoded.iat + 3600;
+    const currentDate = Math.round(new Date().getTime() / 1000);
+    if (expiration < currentDate)
+      throw Error;
+
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
   } catch (error) {
     resp.status(400).json({ error: "Token is not valid" });
   }
