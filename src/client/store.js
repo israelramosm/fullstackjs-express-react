f10ae75a25e39ed7bd64a6e983a2f29d7ac7b8eb
Israel Ramos <arnulfo_israel@hotmail.com>
2021-02-05 22:48:39 -0700

    added react router, redux with first config of store and Login component files

---
+++ src/client/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  // Define a top-level state
+})
+
+const store = createStore(rootReducer);
+
+export default store;
+
+
