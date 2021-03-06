0e6cae38f2151089802f14bb6e9f978184e22f84
Israel Ramos <arnulfo_israel@hotmail.com>
2021-03-05 19:29:27 -0700

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Login from "./content/Login";
 import Main from "./content/Main";
