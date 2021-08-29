268a5bb0e8ea19a2cd3c7de8979734d24c0d4fde
Israel Ramos <arnulfo_israel@hotmail.com>
2021-08-29 18:11:13 -0500

    navbar added, paths and config updated

--- src/App.js
+++ src/App.js
@@ -10,12 +10,57 @@ import './app.scss';
 const App = () => {
   const username = 'username';
   return (
-    <div className="max-width">
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path={`/${username}`} component={Main} />
-        <Route path="/" component={LandingPage} />
-      </Switch>
+    <div className="app">
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/#">
+            Navbar
+          </a>
+          <div className="d-flex">
+            <div className="dropdown dropstart">
+              <button
+                className="btn btn-primary"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i className="bi bi-person-fill bi-1x" />
+              </button>
+              <ul
+                className="dropdown-menu app__dropdown-menu-aligment"
+                aria-labelledby="dropdownMenuButton1"
+              >
+                <li>
+                  <a className="dropdown-item" href="/#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="max-width">
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path={`/${username}`} component={Main} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </div>
     </div>
   );
 };
