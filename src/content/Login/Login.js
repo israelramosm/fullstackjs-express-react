ad18a060cd5b47863da5a0ef291bc5dbe245e207
Israel Ramos <arnulfo_israel@hotmail.com>
2021-08-30 21:54:09 -0500

    login design and layout added

--- src/content/Login/Login.js
+++ src/content/Login/Login.js
@@ -1,5 +1,52 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
-const Login = () => <div>Login</div>;
+import './_login.scss';
+
+const Login = () => (
+  <div className="row login">
+    <div className="col-md-6 login__form">
+      <h2>Login</h2>
+      <form>
+        <label htmlFor="email" className="form-label">
+          Username
+        </label>
+        <div className="input-group mb-3">
+          <input
+            name="email"
+            id="email"
+            type="text"
+            className="form-control"
+            placeholder="Your Email *"
+            aria-label="Your Email"
+          />
+        </div>
+        <label htmlFor="password" className="form-label">
+          Password
+        </label>
+        <div className="input-group mb-3">
+          <input
+            name="password"
+            id="password"
+            type="text"
+            className="form-control"
+            placeholder="Your Pasword *"
+            aria-label="Your Password"
+          />
+        </div>
+        <div className="form-group d-flex justify-content-center mb-3">
+          <button type="button" className="btn btn-primary login__button">
+            Login
+          </button>
+        </div>
+        <div className="form-group d-flex justify-content-center">
+          <a href="/#" className="login__forget-password">
+            Forget Password?
+          </a>
+        </div>
+      </form>
+    </div>
+  </div>
+);
 
 export default Login;
